026d9bb8b76c14988ab019b152f505149cab69a0
stcrodrii <cabralrodrigo793@gmail.com>
2021-08-25 21:09:44 -0300

    desafios4

---
+++ desafio4.java
@@ -0,0 +1,13 @@
+public class desafio4 {
+
+    public static void main(String[] args) {
+    String nombre = "rodrigo";
+    int edad = 26 ; 
+    double salario = 200000.99 ;
+    boolean carnet = true ; 
+    System.out.println(nombre); 
+    System.out.println(edad);
+    System.out.println(salario);
+    System.out.println(carnet); 
+    }
+}
