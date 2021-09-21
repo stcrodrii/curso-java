84d2501df8473157a02ca48843fbe1430376892e
stcrodrii <cabralrodrigo793@gmail.com>
2021-09-21 14:48:45 -0300

    como importar paquetes

---
+++ importar_paquetes/paquetes_y_clases.java
@@ -0,0 +1,21 @@
+package importar_paquetes;
+import java.util.*;     /* asi se importan las clases que no pertenescan al paquete por defecto de java (java.lang)
+ se coloca " import java.util.*; " java.util xq tanto el scanner ciomo el locale pertenecen al paquete de (java.util)
+ al colocar el (*) lo que le decimos es xq me importe todas las clases que trae el paquete o si no tenemos que ir 
+ colocando paquete por paquete a medida que usames alguna clase que no sea del paquete por defecto (java.lang) y para 
+ evitar errores lo mas recomendable es utilizar el (*) ahora bien utilizar el (*) solo consume mas memoria que si 
+ colocamos el paquete invividualmente   */
+
+import java.util.Locale; /* asi se coloca los paquetes de 1 en 1 " import java.util.locale"  import va siempre despues va 
+el paquete donde se encuentra esa clase en este caso es el (java.util) y por ultimo va el nombre del metodo que estamos usando
+en este caso es el metodo (locale) */
+
+
+public class paquetes_y_clases {
+    public static void main(String[] args) {
+        String nombre ;
+        Scanner miobjeto ; 
+        Locale miobjeto2 ; 
+        
+    }
+}
