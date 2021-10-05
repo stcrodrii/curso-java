2d1cb8ea37e926d1582abbde0ad760cf2a477812
stcrodrii <cabralrodrigo793@gmail.com>
2021-10-05 15:27:09 -0300

    definiciones

---
+++ definiciones/CONCEPTOS.java
@@ -0,0 +1,40 @@
+package definiciones;
+public class CONCEPTOS {
+
+/*
+IDENTACION : dejar tabulaciones para diferenciar los cidgos de uno u otro 
+
+REFUNDICION O CASTING : convertir un tipo de dato en otro tipo de dato - ej. double num1 = 7.5;
+                                                                             int num2 = (int) num1;
+
+
+API: libreria donde se encuentran todas las clases sobre java
+
+queit sensitive : es un lenguaje de programacion donde debes respetar las mayusculas y minusculas
+
+CONCATENAR: unir cadenas de caracteres con valores numericos o con variables
+
+ISTANCIAR O EJEMPLAR (clase string)  : (crear un objeto) darle un nombre a un objeto donde almacenaremos luego un valor 
+
+¿QUE QUIERE DECIR QUE UN METODO SEA ESTATICO ? que sea estatico implica que delante del metodo hay que colocar la clase
+ej:JOptionPane.showInputDialog() 
+  (la clase) -      (metodo)
+
+¿QUE QUIERE DECIR QUE UN METODO NO SEA ESTATICO ? quiere decir que en lugar de colocar la clase delante del metodo 
+lo que debemos colocar es un objeto correspondiente a esa clase y luego el metodo el objeto debe estar (ISTANCIADO)
+ej : 
+
+BUCLE DEYERMINDO :  quiere decir que nosotros sabemos cuantas veces se repetira el bucle 
+
+BUCLE IN-DEPERMINADO : quiere decir qwue NO sabemos la cantidad de veces que el bucle se repetira
+
+MODULARIZACION : programas divididos en "trozos " , "partes"
+ , "modulos" o "clases" que se conectan entre si para formar uin todo ... esto es util xq si cometes
+ un error solo buscas en ese bloque el error y no en todo el programa
+
+FINAL : para que las clases no hereden de otras o para qe los metodos no se ocupen con el mismo nombre y hagan otra cosa y tambien sirve para crear constantes
+
+
+*/
+    
+}
